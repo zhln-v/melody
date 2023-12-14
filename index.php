8e584f7634957096111e8ff87e9a8f057268e6ee
zhln-v <58518039+zhln-v@users.noreply.github.com>
2023-12-14 14:37:11 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,113 @@
+<html lang="ru"><head>
+    <meta charset="utf-8">
+	<meta name="generator" content="Joomla! - Open Source Content Management">
+	<title>О компании</title>
+	<link href="/index.php/about?format=feed&amp;type=rss" rel="alternate" type="application/rss+xml" title="О компании">
+	<link href="/index.php/about?format=feed&amp;type=atom" rel="alternate" type="application/atom+xml" title="О компании">
+	<link href="http://localhost/index.php/component/finder/search?format=opensearch&amp;Itemid=103" rel="search" title="OpenSearch melody" type="application/opensearchdescription+xml">
+<link href="/media/vendor/awesomplete/css/awesomplete.css?1.1.5" rel="stylesheet">
+<script src="/media/mod_menu/js/menu.min.js?7d6baf" type="module"></script>
+	<script type="application/json" class="joomla-script-options loaded">{"joomla.jtext":{"MOD_FINDER_SEARCH_VALUE":"Search &hellip;","JLIB_JS_AJAX_ERROR_OTHER":"An error has occurred while fetching the JSON data: HTTP %s status code.","JLIB_JS_AJAX_ERROR_PARSE":"A parse error has occurred while processing the following JSON data:<br><code style=\"color:inherit;white-space:pre-wrap;padding:0;margin:0;border:0;background:inherit;\">%s</code>"},"finder-search":{"url":"/index.php/component/finder/?task=suggestions.suggest&format=json&tmpl=component&Itemid=103"},"system.paths":{"root":"","rootFull":"http://localhost/","base":"","baseFull":"http://localhost/"},"csrf.token":"ea1544787b67d9624f8abe0818d398f0"}</script>
+	<script src="/media/system/js/core.min.js?508d9d"></script>
+	<script src="/media/vendor/webcomponentsjs/js/webcomponents-bundle.min.js?2.8.0" nomodule="" defer=""></script>
+	<script src="/media/com_content/js/articles-list.min.js?c512d2" type="module"></script>
+	<script src="/media/system/js/joomla-hidden-mail.min.js?82da35" type="module"></script>
+	<script src="/media/vendor/awesomplete/js/awesomplete.min.js?1.1.5" defer=""></script>
+	<script src="/media/com_finder/js/finder.min.js?d82ed7" type="module"></script>
+	<script type="application/ld+json">{"@context":"https://schema.org","@graph":[{"@type":"Organization","@id":"http://localhost/#/schema/Organization/base","name":"melody","url":"http://localhost/"},{"@type":"WebSite","@id":"http://localhost/#/schema/WebSite/base","url":"http://localhost/","name":"melody","publisher":{"@id":"http://localhost/#/schema/Organization/base"},"potentialAction":{"@type":"SearchAction","target":"http://localhost/index.php/component/finder/search?q={search_term_string}&amp;Itemid=103","query-input":"required name=search_term_string"}},{"@type":"WebPage","@id":"http://localhost/#/schema/WebPage/base","url":"http://localhost/index.php/about","name":"О компании","isPartOf":{"@id":"http://localhost/#/schema/WebSite/base"},"about":{"@id":"http://localhost/#/schema/Organization/base"},"inLanguage":"en-GB"},{"@type":"Blog","@id":"http://localhost/#/schema/com_content/category/10","isPartOf":{"@id":"http://localhost/#/schema/WebPage/base"},"name":"О компании","blogPost":[{"@id":"http://localhost/#/schema/com_content/article/25"},{"@id":"http://localhost/#/schema/com_content/article/23"},{"@id":"http://localhost/#/schema/com_content/article/24"},{"@id":"http://localhost/#/schema/com_content/article/2"}]},{"@type":"BlogPosting","@id":"http://localhost/#/schema/com_content/article/25","name":"Форма обратной связи","headline":"Форма обратной связи","inLanguage":"en-GB","author":{"@type":"Person","name":"vlad"},"articleSection":"pages","dateCreated":"2023-12-13T23:48:48+00:00"},{"@type":"BlogPosting","@id":"http://localhost/#/schema/com_content/article/23","name":"Контакты","headline":"Контакты","inLanguage":"en-GB","author":{"@type":"Person","name":"vlad"},"articleSection":"pages","dateCreated":"2023-12-13T12:17:27+00:00"},{"@type":"BlogPosting","@id":"http://localhost/#/schema/com_content/article/24","name":"Новое","headline":"Новое","inLanguage":"en-GB","author":{"@type":"Person","name":"vlad"},"thumbnailUrl":"images/banners/22.jpg","articleSection":"pages","dateCreated":"2023-12-13T15:40:09+00:00","interactionStatistic":{"@type":"InteractionCounter","userInteractionCount":10}},{"@type":"BlogPosting","@id":"http://localhost/#/schema/com_content/article/2","name":"MELODY","headline":"MELODY","inLanguage":"en-GB","author":{"@type":"Person","name":"vlad"},"thumbnailUrl":"images/banners/fonstola.ru_140279.jpg","dateCreated":"2023-11-22T21:31:30+00:00"}]}</script>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/styles/main.css">
+    <link rel="stylesheet" href="assets/styles/product.css">
+    <link rel="stylesheet" href="assets/styles/footer.css">
+    <title>Document</title>
+    <script src="assets/scripts/main.js" defer=""></script>
+</head>
+<body>
+    <header>
+        <div class="wrap">
+            <a href="index.html">
+                <div class="logo-wrap">
+                    <img src="assets/icons/logo.png" alt="">
+                    <h2>melody</h2>
+                </div>
+            </a>
+
+            <nav>
+                <div class="nav-search">
+                    <div class="search-wrap">
+                        
+<form class="mod-finder js-finder-searchform form-search" action="/index.php/component/finder/search?Itemid=103" method="get" role="search">
+    <label for="mod-finder-searchword111" class="visually-hidden finder"> </label><div class="mod-finder__search input-group"><div class="awesomplete"><input type="text" name="q" id="mod-finder-searchword111" class="js-finder-search-query form-control" value="" placeholder="Search …" autocomplete="off" aria-expanded="false" aria-owns="awesomplete_list_1" role="combobox"><ul hidden="" role="listbox" id="awesomplete_list_1"></ul><span class="visually-hidden" role="status" aria-live="assertive" aria-atomic="true">Type 2 or more characters for results.</span></div><button class="btn btn-primary" type="submit"><span class="icon-search icon-white" aria-hidden="true"></span> Search</button></div>
+            <input type="hidden" name="Itemid" value="103"></form>
+
+                    </div>
+                </div>
+                <ul class="mod-menu mod-list nav ">
+<li class="nav-item item-106"><a href="index.html#contacts" tabindex="0">Контакты</a></li><li class="nav-item item-103 default"><a href="products.html" tabindex="0">Товары</a></li><li class="nav-item item-104 current active"><a href="index.html" aria-current="page" tabindex="0">О компании</a></li><li class="nav-item item-107"><a href="#" tabindex="0">Вход</a></li></ul>
+
+                <div class="menu-btn">
+                    <img src="assets/icons/menu.png" alt="">
+                </div>
+            </nav>
+        </div>
+    </header>
+
+
+    <main>
+        <div class="wrap">
+                                    <div class="com-content-category category-list">
+
+<div class="content-category">
+    
+        
+                
+        
+	                            <div class="banner">
+                    <h2>MELODY</h2>
+                    <img src="assets/img/22.jpg" alt="">
+                    <div class="simple_markup"><p>Экосистема для музыкантов и лидер розничной продажи музыкальных инструментов и оборудования в России. Наши магазины предлагают лучшие музыкальные инструменты, созданные мировыми производителями, а также весь спектр профессионального звукового, светового и студийного оборудования.</p></div>
+                </div>
+            
+                                <div class="banner" style="margin-top: 10px;">
+                    <h2>Новое</h2>
+                    <img src="assets/img/22.jpg" alt="">
+                    <div class="simple_markup"></div>
+                </div>
+            
+                                <div class="simple_markup">
+                    <h3>Контакты</h3>
+                    <div class="simple_content">
+                        <p id="contacts">Позвоните по номеру <a href="tel:89005900805">8 900 590 08 05</a></p>
+<p>Напишите на почту <a><joomla-hidden-mail is-link="1" is-email="1" first="emhsbi52Mw==" last="Z21haWwuY29t" text="emhsbi52M0BnbWFpbC5jb20=" base=""><a href="mailto:zhln.v3@gmail.com" base="">zhln.v3@gmail.com</a></joomla-hidden-mail></a></p>                    </div>
+                </div>
+
+            
+                                <div class="simple_markup">
+                   <h3>Форма обратной связи</h3>
+                    <div class="simple_content">
+                        <form>
+<h3>Напишите, что думаете о нас</h3>
+<input id="user_first-name" type="text" placeholder="Ваше имя"> <input id="user_email" type="email" placeholder="Ваша почта"> <textarea id="" cols="30" name="" rows="10" placeholder="Тут можно писать всё, что хотите"></textarea> <button>Отправить</button></form>                    </div>
+                </div>
+
+            
+    
+    </div>
+
+</div>
+
+        </div>
+    </main>
+
+    <footer>
+        
+<div id="mod-custom116" class="mod-custom custom">
+    <div class="footer-wrap">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+<p>2023 - Жилин Владислав Романович</p>
+&nbsp; &nbsp; &nbsp; &nbsp;</div></div>
+
+    </footer>
+
+</body></html>
